9f7500557c4880ce098c7787f2478937a5615d39
Alexey Ousov <dax@xdax.ru>
2023-01-15 01:05:50 +0700

    placeholder images

--- prepare-images.mjs
+++ prepare-images.mjs
@@ -33,10 +33,8 @@ export class ImagesList {
             if(err) throw err;
         });
         let offsetx = 0, offsety = 0;
-        for(const [name, imageObject] of this.#images.entries()) {
-            if(!usableImages.has(name))
-                continue;
-            usableImages.delete(name);
+
+        const addImage = (name, imageObject) => {
             const imgx = offsetx * ImagesList.RESOLUTION, imgy = offsety * ImagesList.RESOLUTION;
             resultImage.composite(imageObject, imgx, imgy, Jimp.BLEND_SOURCE_OVER);
             cssContent.push(`.${name} {background-position: ${-imgx}px ${-imgy}px;}`);
@@ -44,9 +42,21 @@ export class ImagesList {
             offsetx = (offsetx + 1) % columns;
             if(!offsetx)
                 offsety += 1;
+        };
+
+        for(const [name, imageObject] of this.#images.entries()) {
+            if(!usableImages.has(name))
+                continue;
+            usableImages.delete(name);
+            addImage(name, imageObject);
         }
         if(usableImages.size) {
             console.log(`Images not created: ${[...usableImages].sort().join(',')}`);
+            for(const name of usableImages) {
+                const letter = name.substring(2, 3);
+                const imageObject = await ImagesList.#createPlaceholderImage(letter);
+                addImage(name, imageObject);
+            }
         }
         resultImage.crop(0, 0, width, (offsety + 1) * ImagesList.RESOLUTION);
         await resultImage.writeAsync(imagePath);
@@ -69,20 +79,35 @@ export class ImagesList {
         const derivedImages = new ImagesList();
         const jsonFile = JSON.parse(await fs.readFile(jsonPath, 'utf8'));
         for(const object of jsonFile.Objects) {
-            this.#_parseAndBuildImages(baseImages, derivedImages, object.Images);
+            await this.#_parseAndBuildImages(baseImages, derivedImages, object.Images);
         }
         return derivedImages;
     }
-    static #_parseAndBuildImages(baseImages, derivedImages, images) {
+    static #IMAGE_PLACEFOLDER = {
+        'T_SteelCombustionEngine': 'SC',
+        'T_AluminiumCombustionEngine': 'AC',
+        'T_StainlessSteelCombustionEngine': 'LC',
+        'T_TitaniumCombustionEngine': 'TC',
+        'T_HardMetalCombustionEngine': 'HC',
+        'T_NeutroniumCombustionEngine': 'NC',
+    };
+    static async #_parseAndBuildImages(baseImages, derivedImages, images) {
+        const baseNotFound = new Set();
         const notFound = new Set();
         for(const image of images) {
+            if(image.Base == 'T_')
+                continue;
             const baseImage = baseImages.getImageObject(image.Base);
             let derivedImage = null;
             if(baseImage) {
                 derivedImage = baseImage.clone();
             } else {
-                notFound.add(image.Base);
-                continue;
+                baseNotFound.add(image.Base);
+                if(this.#IMAGE_PLACEFOLDER[image.NewName]) {
+                    derivedImage = await this.#createPlaceholderImage(this.#IMAGE_PLACEFOLDER[image.NewName]);
+                } else {
+                    continue;
+                }
             }
             if(image.AddMask) {
                 const mask = baseImages.getImageObject(image.AddMask);
@@ -102,8 +127,11 @@ export class ImagesList {
             }
             derivedImages.addImageObject(image.NewName, derivedImage);
         }
+        if(baseNotFound.size) {
+            console.log(`base images not found: ${[...baseNotFound].sort().join(',')}`);
+        }
         if(notFound.size) {
-            console.log(`images not found: ${[...notFound].sort().join(',')}`);
+            console.log(`add images not found: ${[...notFound].sort().join(',')}`);
         }
         return derivedImages;
     }
@@ -122,6 +150,38 @@ export class ImagesList {
         );
         image.mask(bitmask, 0, 0);
         */
+    }
+    static #font16 = null;
+    static #font32 = null;
+    //simulate evospace itself - add one letter image
+    static async #createPlaceholderImage(letters) {
+        let font = null;
+        let yoffset = null;
+        switch(letters.length) {
+            case 1:
+                if(!this.#font32) {
+                    this.#font32 = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+                }
+                font = this.#font32;
+                yoffset = 0;
+                break;
+            case 2:
+                if(!this.#font16) {
+                    this.#font16 = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+                }
+                font = this.#font16;
+                yoffset = 8;
+                break;
+            default:
+                throw new Error(`invalid letters length: ${letters}`);
+        }
+        const width = Jimp.measureText(font, letters);
+        const xoffset = Math.round((ImagesList.RESOLUTION - width) / 2);
+        const placeholderImage = new Jimp(ImagesList.RESOLUTION, ImagesList.RESOLUTION, 0xffffff00, (err) => {
+            if(err) throw err;
+        });
+        placeholderImage.print(font, xoffset, yoffset, letters);
+        return placeholderImage;
     }
 }
 ImagesList.RESOLUTION = 32;
