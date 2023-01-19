d5740a9209e798457f88fb3012d819f097841f00
Alexey Ousov <dax@xdax.ru>
2023-01-20 00:57:22 +0700

    filter by link click

--- site/src/components/main-panel/blueprint-single-io.vue
+++ site/src/components/main-panel/blueprint-single-io.vue
@@ -2,6 +2,7 @@
 import {computed} from 'vue';
 import type {RecipeIOModel} from '@/scripts/model/store';
 import {formatIo} from '@/scripts/format';
+import {injectFilter} from '@/scripts/filter';
 
 const props = defineProps<{
     io: RecipeIOModel;
@@ -11,11 +12,25 @@ const emit = defineEmits<{
     (e: 'link-drag-force'): void;
 }>();
 
+const filter = injectFilter();
+
 function isLtr() {
     return props.io.isFlipped ? !props.io.isInput : props.io.isInput;
 }
 const direction = computed(() => isLtr() ? 'flex-row' : 'flex-row-reverse');
 const align = computed(() => isLtr() ? 'text-left' : 'text-right');
+const border = computed(() => {
+    if(filter.key == props.io.name)
+        return 'highlight-border';
+    return '';
+});
+
+function filterForIo() {
+    if(filter.key != props.io.name) {
+        filter.key = props.io.name;
+        filter.direction = props.io.isInput ? 1 : -1;
+    }
+}
 </script>
 
 <template>
@@ -29,7 +44,8 @@ const align = computed(() => isLtr() ? 'text-left' : 'text-right');
         />
         <div
             class="io-description-row text-caption hover-border"
-            :class="[align]"
+            :class="[align, border]"
+            @click="filterForIo()"
         >
             {{ formatIo(props.io.cpsSolvedTotal, props.io) || '?' }}
             <br>
